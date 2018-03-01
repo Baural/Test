f6317ff1e44f8b75dc2c96d72be2a1b1a77a7c4f
Baural <alzhanovbb@gmail.com>
2018-03-01 23:44:34 +0600

    Change folder

---
+++ Project1/src/abc/MyClass1.java
@@ -0,0 +1,12 @@
+package abc;
+
+/**
+ * Created by baur on 9/18/17.
+ */
+
+public class MyClass1 {
+    public static void main(String[] args) {
+
+        System.out.println("abc");
+    }
+}
---
+++ Project1/src/kz/baur/project/MyClass2.java
@@ -0,0 +1,13 @@
+package kz.baur.project;
+
+import abc.MyClass1;
+
+/**
+ * Created by baur on 9/18/17.
+ */
+public class MyClass2 {
+    public static void main(String[] args) {
+        MyClass1.main(args);
+        System.out.println("main from MyClass2");
+    }
+}
